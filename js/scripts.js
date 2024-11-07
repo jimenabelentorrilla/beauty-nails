bed013bee68f9333fc516bd8854c0bbc1982d0cb
Jimena Belen <jiment4@hotmail.com>
2024-11-07 20:52:28 -0300

    delete services details

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 const flagsElement = document.getElementById("flags");
-
 const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
     const requestJson = await fetch(`../languages/${language}.json`);
     const texts = await requestJson.json();
 
-    for (const textToChange of textsToChange){
+    for (const textToChange of textsToChange) {
         const section = textToChange.dataset.section;
         const value = textToChange.dataset.value;
         textToChange.innerHTML = texts[section][value];
     }
+
+    
+    localStorage.setItem("language", language);
 };
 
+
 flagsElement.addEventListener("click", (e) => {
-    changeLanguage(e.target.parentElement.dataset.language);
+    const language = e.target.parentElement.dataset.language;
+    changeLanguage(language);
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const savedLanguage = localStorage.getItem("language") || "es"; 
+    changeLanguage(savedLanguage);
 });
